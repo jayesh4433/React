68b728a7f345aed483628a22ad81174b28ba75b4
jayesh4433 <gopaljayesh85@gmail.com>
2022-11-27 00:17:08 +0530

    added css

--- basic-app/src/components/ExpenseItem.js
+++ basic-app/src/components/ExpenseItem.js
@@ -1,7 +1,18 @@
-function ExportItem(){
-    return (
-        <h2>Expense Item List</h2>
-    )
+import "./ExpenseItem.css";
+
+function ExportItem() {
+    const expenseDate = new Date(2022,11,23);
+    const expenseTitle = 'Bike Insurance';
+    const expenseAmount = 4130;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExportItem;
