2e1b68907c7f518274d3f3f61955800f0e4bdc55
jayesh4433 <gopaljayesh85@gmail.com>
2023-01-15 12:23:09 +0530

    added year filter

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -3,33 +3,34 @@ import Card from "./components/Card/Card";
 import AddExpense from "./components/AddExpense/AddExpense";
 import ExpensesFilter from "./components/ExpenseFilter/ExpensesFilter";
 import { useState } from "react";
-
+const initailExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
   const [year, setYear] = useState("2020");
-
+  const [expenses, setExpenses] = useState(initailExpenses);
   const addExpenseDataHandler = (expenseData) => {
-    console.log(expenseData);
+    setExpenses((prevExpense) => {
+      return [expenseData, ...prevExpense];
+    });
   };
   const filterYear = (filteredYear) => {
     setYear(filteredYear);
@@ -38,9 +39,8 @@ function App() {
   return (
     <div>
       <AddExpense onAddExpenseDataHandler={addExpenseDataHandler} />
-
       <Card>
-        <ExpensesFilter onFilterYear={filterYear} defaultYear={year}/>
+        <ExpensesFilter onFilterYear={filterYear} defaultYear={year} />
         <Expense expense={expenses} selectedYear={year}></Expense>
       </Card>
     </div>
--- basic-app/src/components/Expenses/Expenses.js
+++ basic-app/src/components/Expenses/Expenses.js
@@ -5,13 +5,20 @@ import "./Expenses.css";
 function expenses(props) {
   const data = props.expense;
   const year = props.selectedYear;
-  console.log(year);
   return (
     <div>
-      <ExportItem item={data[0].title} cost={data[0].amount} date={data[0].date} />
-      <ExportItem item={data[1].title} cost={data[1].amount} date={data[1].date} />
-      <ExportItem item={data[2].title} cost={data[2].amount} date={data[2].date} />
-      <ExportItem item={data[3].title} cost={data[3].amount} date={data[3].date} />
+      {props.expense
+        .filter((exp) => exp.date.getFullYear() == year)
+        .map((exp) => {
+          return (
+            <ExportItem
+              key={exp.id}
+              item={exp.title}
+              cost={exp.amount}
+              date={exp.date}
+            />
+          );
+        })}
     </div>
   );
 }
